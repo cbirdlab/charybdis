3c6501d918e326163fc6a22a21ac74dc67121837
cbird808 <chris.bird@tamucc.edu>
2019-08-16 19:01:56 -0500

    add common names

--- bin/OTU_CVT_addBlast.R
+++ bin/OTU_CVT_addBlast.R
@@ -5,14 +5,19 @@
 args <- commandArgs (TRUE)
 
 # Ensure 4 parameters
-stopifnot (length (args) == 4)
+stopifnot (length (args) == 5)
 
-CVT_FILE <- args[1]     # CVT_FILE <- "/media/Wapuilani/evan/Charybdis_Runs/MaryJones/out/jones.16S.OTU.CVT"
-BLAST_FILE <- args[2]   # BLAST_FILE <- "/media/Wapuilani/evan/Charybdis_Runs/MaryJones/out/jones.16S.OTU.blastOut"
-SOURCE_NAME <- args[3]  # SOURCE_NAME <- "NCBI-custom"
-OUTPUT_FILE <- args[4]  # OUTPUT_FILE <- "/media/Wapuilani/evan/Charybdis_Runs/MaryJones/out/jones.16S.OTU.CVT-EXT"
+CVT_FILE <- args[1]     #CVT_FILE  <- "out/Drymon-Fall2018.OTUvsTubes.blast.SP.csv"
+BLAST_FILE <- args[2]   # BLAST_FILE <- "out/Drymon-Fall2018.OTU.blastOut"
+SOURCE_NAME <- args[3]  # SOURCE_NAME <- "NCBI"
+OUTPUT_FILE <- args[4]  # OUTPUT_FILE <- "out/Drymon-Fall2018.OTUvsTubes.blast.SPA.csv"
+COMMON_NAMES_FILE <- args[5] # COMMON_NAMES_FILE <- "common_names.txt"
 
 CVT <- read.csv (file = CVT_FILE, header = TRUE, stringsAsFactors = FALSE)
+COMMON_NAMES <- read.csv(COMMON_NAMES_FILE, header = FALSE, stringsAsFactors = FALSE)
+names(COMMON_NAMES) <- "CommonName"
+CVT<-data.frame(cbind(CVT[,2:3],COMMON_NAMES, CVT[,4:length(names(CVT))]))
+
 BLAST <- read.csv (file = BLAST_FILE, header = FALSE, stringsAsFactors = FALSE)
 
 blast_columns <- c ("OTU_SEQID", "SSEQID", "SSCINAMES", "STAXIDS", paste0("BLAST_E-SCORE_", SOURCE_NAME),
@@ -34,14 +39,11 @@ CVT_BLAST$SSCINAMES = NULL
 CVT_BLAST$STAXIDS = NULL
 write.csv (x = CVT_BLAST, quote = FALSE, file = OUTPUT_FILE)
 
-#clean up this file
-CVT_BLAST$X <- NULL
-
-col_names <- colnames(CVT_BLAST)
-startcol <- which(col_names == "superorder")
-endcol <- which(col_names == "species.group")
-CVT_BLAST[,startcol:endcol] <- NULL
-
-write.csv (x = CVT_BLAST, quote = FALSE, file = paste(OUTPUT_FILE,"cleaner.csv", sep="."))
+##clean up this file
+#col_names <- colnames(CVT_BLAST)
+#startcol <- which(col_names == "superorder")
+#endcol <- which(col_names == "species.group")
+#CVT_BLAST[,startcol:endcol] <- NULL
+#write.csv (x = CVT_BLAST, quote = FALSE, file = paste(OUTPUT_FILE,"cleaner.csv", sep="."))
 
                       
